fa5304e273866030abd7ce907c5834cdc2d73992
eaniko <69327188+eaniko@users.noreply.github.com>
2022-02-16 19:25:11 +0100

    kész

---
+++ src/app/common/data-cell/data-cell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-cell',
+  templateUrl: './data-cell.component.html',
+  styleUrls: ['./data-cell.component.scss']
+})
+export class DataCellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/common/icon/icon.component.ts
+++ src/app/common/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon.component.scss']
 })
 export class IconComponent implements OnInit {
+
+  @Input() icon: string = '';
 
   constructor() { }
 
